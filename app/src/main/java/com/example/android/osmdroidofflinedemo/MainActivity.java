82663eb9492935f924158f3d232152d048125f44
RH <52927295@qq.com>
2018-11-02 13:26:29 +0800

    配置以及资源
    更改本地配置
    地图位置中心点更改
    地图资源问题

--- app/src/main/java/com/example/android/osmdroidofflinedemo/MainActivity.java
+++ app/src/main/java/com/example/android/osmdroidofflinedemo/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.android.osmdroidofflinedemo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class MainActivity extends Activity {
 
-    public static final GeoPoint BERLIN = new GeoPoint(52.516667, 13.383333);
+    public static final GeoPoint BERLIN = new GeoPoint(31.2365071255,121.4665718296);
+    public static final OnlineTileSourceBase LOCAL_OSM_FILES = new XYTileSource("4uMaps",
+            ResourceProxy.string.mapquest_osm, 0, 15, 256, ".png", new String[] { });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,10 @@ public class MainActivity extends Activity {
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
         mapView.setUseDataConnection(true);
-        mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
-
+        //mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+        mapView.setTileSource(LOCAL_OSM_FILES);
         IMapController mapViewController = mapView.getController();
-        mapViewController.setZoom(15);
+        mapViewController.setZoom(6);
         mapViewController.setCenter(BERLIN);
     }
 
